81ca590d3db47a130f14eae0f779256ff24068b2
Swarup Chanda <swarupchanda1963@gmail.com>
2025-10-12 17:26:54 +0530

    Updates

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,14 +4,14 @@ import WhatDoWeProvideSlider from "../../components/what_do_we_provide/what_do_w
 import Projects from "../../components/Projects/Projects";
 import AboutUsSection from "../../components/About_Us/AboutUsSection";
 import Upcoming_events from "../../components/upcoming_events/event_card_slider";
-import Hero from "../../components/Hero/Landing";
+import Galaxy from "../../components/Hero/Galaxy";
 
 
 const Home = () => {
   return (
     <div className="overflow-x-hidden">
       
-      <Hero/>
+      <Galaxy/>
       <Upcoming_events/>
       <AboutUsSection></AboutUsSection>
       <Projects />
