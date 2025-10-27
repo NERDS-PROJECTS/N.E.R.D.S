9d1d7c754804a5b94b91ff4aadd14a5ce180ad4e
Swarup Chanda <swarupchanda1963@gmail.com>
2025-10-27 21:03:33 +0530

    Upda6tes

--- src/components/Hero/Landing.jsx
+++ src/components/Hero/Landing.jsx
@@ -1,37 +1,35 @@
 import './landing.css';
-import StarCanvas from "../../assets/canvas/Stars";
-import { SplineScene } from "@/components/ui/splite";
+import StarCanvas from "../../assets/canvas/Stars"
 
 const Landing = () => {
- return(
-    <>
+    return (
+        <>
 
-    <div className="main-div">
-    <StarCanvas/>
-    <h1 className="typewriter font-spaced">ROBOTICS CLUB, NIT SILCHAR</h1>
+            <div className="main-div">
+                <StarCanvas />
+                <div className="flex items-center justify-center h-screen w-full bg-black">
+                    <h1 className="typewriter font-spaced text-white text-3xl md:text-5xl text-center">
+                        ROBOTICS CLUB, NIT SILCHAR
+                    </h1>
+                </div>
 
-        <div className='parallax-image'>
-          <SplineScene 
-            scene="https://prod.spline.design/kZDDjO5HuC9GJUM2/scene.splinecode"
-            className="w-full h-full"
-          />
-        </div>
-        <div className='nerds parallax-text font-ethenocentric text-[#b8b8b8]'>
-            <h1>N</h1>
-            <h1>.</h1>
-            <h1>E</h1>
-            <h1>.</h1>
-            <h1>R</h1>
-            <h1>.</h1>
-            <h1>D</h1>
-            <h1>.</h1>
-            <h1>S</h1>
+                <img src="https://res.cloudinary.com/dmhbmurzw/image/upload/v1728710649/Subject_lvavbf.png" alt="Hero-img" className='parallax-image' />
+                <div className='nerds parallax-text font-ethenocentric text-[#b8b8b8]'>
+                    <h1>N</h1>
+                    <h1>.</h1>
+                    <h1>E</h1>
+                    <h1>.</h1>
+                    <h1>R</h1>
+                    <h1>.</h1>
+                    <h1>D</h1>
+                    <h1>.</h1>
+                    <h1>S</h1>
 
-        </div>
+                </div>
 
-    </div>
+            </div>
 
-    </>
- )
+        </>
+    )
 };
 export default Landing;
